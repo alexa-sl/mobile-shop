b455fc5ddd3ef0ef36c4527a48487e0476f81837
alexa-sl <>
2023-07-24 13:42:38 +0300

    Layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CartPageComponent } from './cart-page/cart-page.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { ProductPageComponent } from './product-page/product-page.component';
+import { MainLayoutComponent } from './shared/main-layout/main-layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CartPageComponent,
+    MainPageComponent,
+    ProductPageComponent,
+    MainLayoutComponent
   ],
   imports: [
     BrowserModule,
